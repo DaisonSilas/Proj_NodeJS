3fb7e2d24582bc10db4029c4a974b0fb0cc33472
Daison Silas Oliveira de Brito <48499657+DaisonSilas@users.noreply.github.com>
2025-11-25 20:43:03 -0300

    Atualizando Server v2

--- server.js
+++ server.js
@@ -1,21 +1,16 @@
 import express from "express";
-import usersRoutes from "./routes/users.js";
 import productsRoutes from "./routes/products.js";
+import usersRoutes from "./routes/users.js";
 import { log } from "./middlewares/log.js";
 
 const app = express();
 
-// Middleware para interpretar JSON (importante)
 app.use(express.json());
-
-// Middleware de log
 app.use(log);
 
-// Rotas
-app.use("/users", usersRoutes);
 app.use("/products", productsRoutes);
+app.use("/users", usersRoutes);
 
-// Inicialização
 app.listen(3000, () => {
   console.log("Servidor rodando na porta 3000");
 });
