bf7ecdd221bc677845d5d4114e796bd3b3465985
Daison Silas Oliveira de Brito <48499657+DaisonSilas@users.noreply.github.com>
2025-11-28 19:38:52 -0300

    Agora criando logs e atualizando middlewares

--- middlewares/log.js
+++ middlewares/log.js
@@ -1,4 +1,10 @@
 export function log(req, res, next) {
-  console.log(`${req.method} ${req.url}`);
+  const logMessage = `[${new Date().toISOString()}] ${req.method} ${req.url}
+Body: ${JSON.stringify(req.body)}
+Params: ${JSON.stringify(req.params)}
+`;
+
+  console.log(logMessage);
+
   next();
 }
