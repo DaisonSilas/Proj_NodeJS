4034f609b6aae25973f228dc355dc226fbb2a621
Daison Silas Oliveira de Brito <48499657+DaisonSilas@users.noreply.github.com>
2025-11-30 18:08:54 -0300

    Atualizando Server

--- srs/server.js
+++ srs/server.js
@@ -1,51 +1,16 @@
 import express from "express";
-import morgan from "morgan";
-import fs from "fs";
-import path from "path";
-import { LOGS_PATH } from "./config/paths.js";
+import productsRoutes from "./routes/products.js";
 import usersRoutes from "./routes/users.js";
-import { log } from "./middlewares/log.js";
-import { router as productsRoutes } from "./routes/products.js";
-
-
+import { errorHandler } from "./middlewares/errorHandler.js";
 
 const app = express();
-
-
-// Aceitar JSON
 app.use(express.json());
-
-
-// Garantir pasta logs
-if (!fs.existsSync(LOGS_PATH)) {
-fs.mkdirSync(LOGS_PATH);
-}
-
-
-// Stream para escrita de logs
-const accessLogStream = fs.createWriteStream(
-path.join(LOGS_PATH, "access.log"),
-{ flags: "a" }
-);
-
-
-// Morgan (arquivo)
-app.use(morgan("combined", { stream: accessLogStream }));
-
-
-// Morgan (console)
-app.use(morgan("dev"));
-
-
-// Log personalizado
-app.use(log);
-
 
 // Rotas
 app.use("/products", productsRoutes);
 app.use("/users", usersRoutes);
 
+// Middleware de erro (sempre no fim)
+app.use(errorHandler);
 
-app.listen(3000, () => {
-console.log("Servidor rodando na porta 3000");
-});
+app.listen(3000, () => console.log("Servidor rodando!"));
