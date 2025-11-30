a10dace5859a2df3c83290a093211e8ce0dfbd5d
Daison Silas Oliveira de Brito <48499657+DaisonSilas@users.noreply.github.com>
2025-11-30 11:49:45 -0300

    Aprimorando o codigo, ajust na server.js

--- server.js
+++ server.js
@@ -1,53 +1,8 @@
-import express from "express";
-import productsRoutes from "./routes/products.js";
-import usersRoutes from "./routes/users.js";
-import { log } from "./middlewares/log.js";
+// src/server.js
+import app from "./app.js";
 
-import morgan from "morgan";
-import fs from "fs";
-import path from "path";
+const PORT = process.env.PORT || 3000;
 
-const app = express();
-
-// Middleware para aceitar JSON
-app.use(express.json());
-
-// ================================
-// LOG - MORGAN
-// ================================
-
-// Criar pasta logs caso não exista
-const logsPath = path.join(process.cwd(), "logs");
-if (!fs.existsSync(logsPath)) {
-  fs.mkdirSync(logsPath);
-}
-
-// Cria o stream de escrita para o arquivo de log
-const accessLogStream = fs.createWriteStream(
-  path.join(logsPath, "access.log"),
-  { flags: "a" } // 'a' = append
-);
-
-// Log em arquivo (formato "combined" é padrão NGINX/Apache)
-app.use(morgan("combined", { stream: accessLogStream }));
-
-// Log no console
-app.use(morgan("dev"));
-
-// ================================
-// LOG PERSONALIZADO (SEU MIDDLEWARE)
-// ================================
-app.use(log);
-
-// ================================
-// ROTAS
-// ================================
-app.use("/products", productsRoutes);
-app.use("/users", usersRoutes);
-
-// ================================
-// SERVIDOR
-// ================================
-app.listen(3000, () => {
-  console.log("Servidor rodando na porta 3000");
+app.listen(PORT, () => {
+  console.log(`🔥 Servidor rodando na porta ${PORT}`);
 });
