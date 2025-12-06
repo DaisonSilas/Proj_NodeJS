16d619320e30cedbe83ca149b1401c3ad7c3fa01
Daison Silas Oliveira de Brito <48499657+DaisonSilas@users.noreply.github.com>
2025-12-06 19:32:36 -0300

    Ajuste de logger

--- srs/app.js
+++ srs/app.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 
-import { loggerFile, loggerConsole } from "./config/logger.js";
+import { logger } from "./config/logger.js";
 import { log } from "./middlewares/log.js";
 import productsRoutes from "./routes/products.js";
 import usersRoutes from "./routes/users.js";
@@ -11,8 +11,13 @@ const app = express();
 
 app.use(express.json());
 
-app.use(loggerFile);
-app.use(loggerConsole);
+// Logger para cada request
+app.use((req, res, next) => {
+  logger.info(`${req.method} ${req.url}`);
+  next();
+});
+
+// Seu log customizado
 app.use(log);
 
 // Rotas
--- srs/server.js
+++ srs/server.js
@@ -2,9 +2,14 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import app from "./app.js"; // <-- importa o app completo
+import app from "./app.js";               // importa o app
+import { logger } from "./config/logger.js"; // importa o logger AQUI
 
-// Porta definida no .env ou fallback para 3000
+// Agora sim você pode usar o logger
+logger.info("Servidor iniciado...");
+logger.error("Erro no servidor...");
+
+// Porta definida no .env ou padrão 3000
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
